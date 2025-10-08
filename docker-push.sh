e22ccdded5d0d0919a1292f391d90b0eb346c403
Gabriel Carvalho <gabrielrenatosc@gmail.com>
2025-10-08 11:37:48 -0300

    feat: ajusta o usuario autenticado

--- docker-push.sh
+++ docker-push.sh
@@ -1,7 +1,7 @@
 set -e
 docker login
 
-DOCKER_USERNAME=${1:-"gabrielrenato3"}
+DOCKER_USERNAME=${1:-"vestibulize"}
 VERSION=${2:-"latest"}
 PROJECT_NAME="vestibulize"
 
@@ -14,31 +14,31 @@ echo "Docker login realizado com sucesso!"
 
 # Build das imagens
 echo "Building backend image..."
-docker build -t $DOCKER_USERNAME/vestibulize-back-api:$VERSION ../back-api
-docker build -t $DOCKER_USERNAME/vestibulize-back-api:latest ../back-api
+docker build -t $DOCKER_USERNAME/back-api:$VERSION ../back-api
+docker build -t $DOCKER_USERNAME/back-api:latest ../back-api
 
 echo "Building frontend image..."
-docker build --build-arg VITE_API_URL=http://localhost:8080 -t $DOCKER_USERNAME/vestibulize-front-web:$VERSION ../front-web
-docker build --build-arg VITE_API_URL=http://localhost:8080 -t $DOCKER_USERNAME/vestibulize-front-web:latest ../front-web
+docker build --build-arg VITE_API_URL=http://localhost:8080 -t $DOCKER_USERNAME/front-web:$VERSION ../front-web
+docker build --build-arg VITE_API_URL=http://localhost:8080 -t $DOCKER_USERNAME/front-web:latest ../front-web
 
 # Push das imagens
 echo "Pushing backend image..."
-docker push $DOCKER_USERNAME/vestibulize-back-api:$VERSION
-docker push $DOCKER_USERNAME/vestibulize-back-api:latest
+docker push $DOCKER_USERNAME/back-api:$VERSION
+docker push $DOCKER_USERNAME/back-api:latest
 
 echo "Pushing frontend image..."
-docker push $DOCKER_USERNAME/vestibulize-front-web:$VERSION
-docker push $DOCKER_USERNAME/vestibulize-front-web:latest
+docker push $DOCKER_USERNAME/front-web:$VERSION
+docker push $DOCKER_USERNAME/front-web:latest
 
 echo ""
 echo "Images pushed successfully!"
 echo ""
 echo "Image URLs:"
-echo "Backend:  docker.io/$DOCKER_USERNAME/vestibulize-back-api:$VERSION"
-echo "Frontend: docker.io/$DOCKER_USERNAME/vestibulize-front-web:$VERSION"
+echo "Backend:  docker.io/$DOCKER_USERNAME/back-api:$VERSION"
+echo "Frontend: docker.io/$DOCKER_USERNAME/front-web:$VERSION"
 echo ""
 echo "To use these images, update your docker-compose.yml with:"
 echo "  backend:"
-echo "    image: $DOCKER_USERNAME/vestibulize-back-api:$VERSION"
+echo "    image: $DOCKER_USERNAME/back-api:$VERSION"
 echo "  frontend:"
-echo "    image: $DOCKER_USERNAME/vestibulize-front-web:$VERSION"
+echo "    image: $DOCKER_USERNAME/front-web:$VERSION"
